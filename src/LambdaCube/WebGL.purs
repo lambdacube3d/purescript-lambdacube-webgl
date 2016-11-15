e61429f6b5fdd7d007628707c05ec96c7c3aa245
Csaba Hruska <csaba.hruska@gmail.com>
2016-11-15 13:41:14 +0100

    add top level module

---
+++ src/LambdaCube/WebGL.purs
@@ -0,0 +1,15 @@
+module LambdaCube.WebGL
+  ( module LambdaCube.IR
+  , module LambdaCube.LinearBase
+  , module LambdaCube.WebGL.Type
+  , module LambdaCube.WebGL.Backend
+  , module LambdaCube.WebGL.Data
+  , module LambdaCube.WebGL.Input
+  ) where
+
+import LambdaCube.IR
+import LambdaCube.LinearBase
+import LambdaCube.WebGL.Type
+import LambdaCube.WebGL.Backend
+import LambdaCube.WebGL.Data
+import LambdaCube.WebGL.Input
